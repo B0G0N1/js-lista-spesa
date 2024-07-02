477aba43ac723072bdcb10acb399acb4fe193460
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-02 23:33:41 +0200

    [UPD] Update JS code with li items

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 let shoppingList = ["pane", "latte", "uova", "frutta", "verdure"];
 
+let ul = document.getElementById("shopping-list");
+
 let i = 0;
 while (i < shoppingList.length) {
-    console.log(shoppingList[i]);
+    let li = document.createElement("li");
+    li.textContent = shoppingList[i];
+    
+    ul.appendChild(li);
+
     i++;
 }
